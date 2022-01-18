edfb3280621b16a61181309e427398c40f1cecf3
kabirswe <kabir.swe@gmail.com>
2022-01-18 22:53:19 +0600

    home page working

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -3,6 +3,11 @@
 
 @section('content')
 <main class="main-container">
-
+    <div class="video-container">
+        <video autoplay muted loop id="myVideo">
+            <source src="https://youtu.be/G1nZNpPBz1o" type="video/mp4">
+            Your browser does not support HTML5 video.
+        </video>
+    </div>
 </main>
 @endsection
