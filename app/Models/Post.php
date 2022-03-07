17f0cb9f46f2755cded094fdbce333e0bd8fc300
kabirswe <kabir.swe@gmail.com>
2022-03-08 00:56:53 +0600

    post delete done

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    use HasFactory, Sluggable;
+    use SoftDeletes, HasFactory, Sluggable;
     protected $fillable = [
         'title',
         'slug',
